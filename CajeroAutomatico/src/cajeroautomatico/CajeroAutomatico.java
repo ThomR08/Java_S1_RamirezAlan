794db19ca2192f91cc002763cd88c54419b44b01
Alan Ramirez (Local Ubuntu) <thomasramirez0806@gmail.com>
2026-01-09 18:48:34 -0500

    feat :sparkles: Add files

---
+++ CajeroAutomatico/src/cajeroautomatico/CajeroAutomatico.java
@@ -0,0 +1,21 @@
+package cajeroautomatico;
+
+public class CajeroAutomatico {
+    
+    public static void main(String[] args) {
+        
+        // Definir Credenciales
+        String numero_cuenta_debito = "5300";
+        String clave_cuenta_debito = "123";
+        String numero_cuenta_credito = "1013";
+        String clave_cuenta_credito = "123";
+        
+        int opciones_menu = 0;
+        
+        // Definir el Menu
+        
+        
+        
+    }
+    
+}
